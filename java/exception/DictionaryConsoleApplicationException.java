f7dd6d0831c5c28b7d1109c8fe1acbe1d198c821
ArinaArtiukevich <71898919+ArinaArtiukevich@users.noreply.github.com>
2021-08-16 14:51:33 +0300

    Delete java directory

--- java/exception/DictionaryConsoleApplicationException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class DictionaryConsoleApplicationException extends Exception{
-    public DictionaryConsoleApplicationException(String msg) {
-        super(msg);
-    }
-}
--- java/exception/ValidatorException.java
+++
@@ -1,7 +0,0 @@
-package exception;
-
-public class ValidatorException extends Exception {
-    public ValidatorException(String msg) {
-        super(msg);
-    }
-}
